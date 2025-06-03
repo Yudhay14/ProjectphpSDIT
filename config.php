b53d51e0f4faf679108059858b31b79aa8e6fcdd
Yudhay14 <wira44089@gmail.com>
2025-06-03 22:00:59 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,48 @@
+<?php
+
+$koneksi = mysqli_connect("localhost", "root", "", "db_sekolah");
+
+//cek koneksi
+// if (mysqli_connect_errno()) {
+//     # code...
+//     echo "Gagal koneksi";
+// } else{
+//     echo "Berhasil";
+// }
+
+//simpan url 
+$main_url = "http://localhost/Project_sekolah_SDIT/";
+
+function uploadimg($url){
+    $namafile = $_FILES['image']['name'];
+    $ukuran   = $_FILES['image']['size'];
+    $error    = $_FILES['image']['error'];
+    $tmp      = $_FILES['image']['tmp_name'];
+
+
+    //cekfileyang di upload 
+    $validextension = ['jpg', 'jpeg', 'png'];
+    $fileExtension  = explode('.', $namafile);
+    $fileExtension  = strtolower(end($fileExtension));
+    if (!in_array ($fileExtension, $validextension)){
+        header("location:" . $url . '?msg=notimage');
+        die;
+    }
+
+    //cek ukuran gambar
+    if($ukuran > 1000000) {
+        header("location:". $url . '?msg=oversize');
+        die;
+    }
+    //generate nama file gMBr
+    $namafilebaru = rand(10, 1000) . '-' . $namafile;
+
+    //upload gmbar
+    move_uploaded_file($tmp, "../asset/image/" . $namafilebaru);
+    return $namafilebaru;
+
+
+
+}
+
+?>
---
+++ template/navbar.php
@@ -0,0 +1,29 @@
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-custom">
+        <!-- Navbar Brand-->
+        <a href="" class="m-3"><input type="image" src="<?= $main_url ?>asset/image/logohome.png" alt=""></a>
+        <a class="navbar-brand text-start fw-bold" href="<?= $main_url ?>index.php">SDIT As-Salam</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars fa-2x"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            <span class="text-white text-capitalize"><?= "Wira Yudha"  ?></span>
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">Profile User</a></li>
+                    <li><a class="dropdown-item" href="#!">Profile Sekolah</a></li>
+                    <li>
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li><a class="dropdown-item" href="#!">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+
+    </nav>
